a2fcd668d3a9d84dede3a05c0bb9cb454737b8b7
Travis Whitaker <pi.boy.travis@gmail.com>
2018-02-05 00:01:03 -0800

    Fix build on macOS.

--- default.nix
+++ default.nix
@@ -1,25 +1,33 @@
 with import <nixpkgs> {};
 
-runCommand "gaugereader-env" rec
+haskell.lib.buildStackProject rec
 {
+    name = "gaugereader";
+
+    ghc = haskell.compiler.ghc822;
+
     libs = [ git
              llvm_5
              libffi
              ffmpeg-full
              zlib
            ] ++ (if stdenv.isDarwin
-                 then []
+                 then [ darwin.cf-private
+                        darwin.apple_sdk.frameworks.CoreFoundation
+                        darwin.apple_sdk.frameworks.Kernel
+                        darwin.apple_sdk.frameworks.Cocoa
+                      ]
                  else [ cudatoolkit8.lib
                         cudatoolkit8.out
                         linuxPackages.nvidia_x11
                       ]
                 );
 
-    buildInputs = [ gcc
+    buildInputs = [ clang
+                    gcc
                     haskell.compiler.ghc822
                     stack
                     pkgconfig
+                    which
                   ] ++ libs;
-
-    LD_LIBRARY_PATH = stdenv.lib.makeLibraryPath libs;
-} ""
+}
